de1d589c9ea6cccba9472d2efe3047781c13ed2d
JP <johnarpaul@pm.me>
2026-08-15 16:36:10 +0100

    Adjust v2 footer spacing

--- app/v2/page.tsx
+++ app/v2/page.tsx
@@ -331,8 +331,8 @@ export default function V2Page() {
           </div>
         </section> */}
 
-        <footer className="mt-16 flex justify-end p-3">
-          <p className="italic text-[11px] text-grayscale-8">Per gratiam dei</p>
+        <footer className="mt-8 flex justify-end p-3">
+          <p className="italic text-[11px] text-grayscale-9">Per gratiam dei</p>
         </footer>
       </div>
     </main>
