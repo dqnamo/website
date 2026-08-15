483f783a868ad41dfa3259e0bd28edabb356d4c9
JP <johnarpaul@pm.me>
2026-08-15 16:31:55 +0100

    Add more links section

--- app/v2/page.tsx
+++ app/v2/page.tsx
@@ -1,4 +1,9 @@
-import { ArrowRightIcon } from "@phosphor-icons/react/dist/ssr";
+import {
+  ArrowRightIcon,
+  GithubLogoIcon,
+  PhoneCallIcon,
+  TwitterLogoIcon,
+} from "@phosphor-icons/react/dist/ssr";
 import Image from "next/image";
 import Link from "next/link";
 import { ExperimentPreview } from "@/components/NewExperimentCta";
@@ -233,6 +238,43 @@ export default function V2Page() {
                 </div>
               </Link>
             ))}
+          </div>
+        </section>
+
+        <section className="mt-16">
+          <div className="p-3">
+            <h2 className="font-medium text-grayscale-12 text-sm">More</h2>
+          </div>
+          <div className="mt-3 grid grid-cols-3 gap-1.5 rounded-[16px] border border-grayscale-3 bg-grayscale-2 p-1.5">
+            <a
+              className="flex min-h-32 flex-col justify-between rounded-[13px] border border-grayscale-3 bg-grayscale-1 p-3 small-shadow transition-colors hover:border-grayscale-4 hover:bg-grayscale-2 dark:border-grayscale-4 dark:bg-grayscale-3 dark:shadow-none dark:hover:border-grayscale-6 dark:hover:bg-grayscale-4"
+              href="https://x.com/dqnamo"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <TwitterLogoIcon aria-hidden="true" size={20} weight="fill" />
+              <h3 className="font-medium text-grayscale-12 text-sm">Twitter</h3>
+            </a>
+            <a
+              className="flex min-h-32 flex-col justify-between rounded-[13px] border border-grayscale-3 bg-grayscale-1 p-3 small-shadow transition-colors hover:border-grayscale-4 hover:bg-grayscale-2 dark:border-grayscale-4 dark:bg-grayscale-3 dark:shadow-none dark:hover:border-grayscale-6 dark:hover:bg-grayscale-4"
+              href="https://github.com/dqnamo"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <GithubLogoIcon aria-hidden="true" size={20} weight="fill" />
+              <h3 className="font-medium text-grayscale-12 text-sm">GitHub</h3>
+            </a>
+            <a
+              className="flex min-h-32 flex-col justify-between rounded-[13px] border border-grayscale-3 bg-grayscale-1 p-3 small-shadow transition-colors hover:border-grayscale-4 hover:bg-grayscale-2 dark:border-grayscale-4 dark:bg-grayscale-3 dark:shadow-none dark:hover:border-grayscale-6 dark:hover:bg-grayscale-4"
+              href="https://cal.com/interface.london/20min"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <PhoneCallIcon aria-hidden="true" size={20} weight="fill" />
+              <h3 className="font-medium text-grayscale-12 text-sm">
+                Book a call
+              </h3>
+            </a>
           </div>
         </section>
 
